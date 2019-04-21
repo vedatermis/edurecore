13d9b6da4d641e7702b210c40a2cb9f8df956be7
Vedat ERMIS <vedatermis@hotmail.com>
2019-04-21 18:55:18 +0300

    commit

--- Edura.WebUI/Controllers/ProductController.cs
+++ Edura.WebUI/Controllers/ProductController.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using Edura.WebUI.Models;
+using Edura.WebUI.Repository.Abstract;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Edura.WebUI.Controllers
 {
     public class ProductController : Controller
     {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -15,7 +22,21 @@ namespace Edura.WebUI.Controllers
 
         public IActionResult Details(int id)
         {
-            return View();
+            return View(_productRepository
+                .GetAll()
+                .Where(i => i.Id == id)
+                .Include(i => i.Images)
+                .Include(i => i.Attributes)
+                .Include(i => i.ProductCategories)
+                .ThenInclude(i => i.Category)
+                .Select(i => new ProductDetailsModel
+                {
+                    Product = i,
+                    Categories = i.ProductCategories.Select(a => a.Category).ToList(),
+                    ProductImages = i.Images,
+                    ProductAttributes = i.Attributes
+                }).FirstOrDefault()
+            );
         }
     }
 }
---
+++ Edura.WebUI/Models/ProductDetailsModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Edura.WebUI.Entity;
+
+namespace Edura.WebUI.Models
+{
+    public class ProductDetailsModel
+    {
+        public Product Product { get; set; }
+        public List<Image> ProductImages { get; set; }
+        public List<ProductAttribute> ProductAttributes { get; set; }
+        public List<Category> Categories { get; set; }
+
+    }
+}
